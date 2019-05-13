6d277faaf4cf361b8b918788d33c21bc4e2ff7fc
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2019-05-13 12:25:41 -0500

    Add files via upload

---
+++ WEB-INF/Classes/lib/preferences.php
@@ -0,0 +1,20 @@
+<?php
+
+/****************************************************************************/
+/*                                                                          */
+/* YOU MAY WISH TO MODIFY OR REMOVE THE FOLLOWING LINES WHICH SET DEFAULTS  */
+/*                                                                          */
+/****************************************************************************/
+
+// Sets the default charset so that setCharset() is not needed elsewhere
+Swift_Preferences::getInstance()->setCharset('utf-8');
+
+// Without these lines the default caching mechanism is "array" but this uses
+// a lot of memory.
+// If possible, use a disk cache to enable attaching large attachments etc
+if (function_exists('./files') && is_writable('./files'))
+{
+  Swift_Preferences::getInstance()
+    -> setTempDir('./files')
+    -> setCacheType('disk');
+}
