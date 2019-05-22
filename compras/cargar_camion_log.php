3b385d555a649b69fd447fc94c4e32da3cf38a6d
Miguel Montiel <31743982+miguelmontiel30@users.noreply.github.com>
2019-05-22 09:55:46 -0500

    Cambios en UI (#1)
    * Cambios en UI
    
    Se agregaron cambios en los botones.
    Se agregaron las librerías de DataTable más recientes al sistema
    
    * Arreglo en las librerías de DataTable
    
    * Se corrigió las rutas que causaban problemas.
    * Se agregó por separado la funcionalidad de la DT con el archivo "resources/compras/logistica.js".
    * Se creó una carpeta en resources (resources/compras)que contendrá los archivos JS para la funcionalidad de la DT.

---
+++ compras/cargar_camion_log.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <!-- ADICIÓN DE METATAGS PARA WEB RESPONSIVE -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- FIN DE LA ADICIÓN DE METATAGS PARA WEB RESPONSIVE -->    
+
+    <title><?php echo $_SESSION['nombreEmpresa']; ?></title>      
+
+    <!-- LINK HACIA BOOTSTRAP, ESTILOS DE LA SIDENAV E ICONOS FONTAWESOME -->
+    <link href="resources/css/Bootstrap 4/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="resources/css/Bootstrap 4/css/all.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <!-- CONTENEDOR PRINCIPAL DE LA VISTA -->
+    <div class="container">        
+
+        <!-- CONTENEDOR DE LA TABLA -->
+        <div class="table-responsive mt-4">
+            
+            <table id="tabla_rutas" class="table table-hover">
+                
+                <!-- CABECERAS DE LA TABLA -->
+                <thead>
+            
+                    <tr>
+
+                        <th  class="text-center" scope="col">Fecha</th>
+                        <th  class="text-center" scope="col">Ruta</th>
+                        <th  class="text-center" scope="col">Destino</th>
+                        <th  class="text-center" scope="col">Pedido</th>
+                        <th  class="text-center" scope="col">Etiqueta</th>
+                        <th  class="text-center" scope="col">Clave de Embarque</th>
+                        <th  class="text-center" scope="col">No. de Control Vehicular</th>
+                        <th  class="text-center" scope="col">Suma de Piezas</th>                        
+                        <th  class="text-center" scope="col">Piezas en camión</th>
+                        <th  class="text-center" scope="col">Piezas Entregadas</th>
+                        <th  class="text-center" scope="col">Status del pedido</th>
+                        <th  class="text-center" scope="col">Cargar Camión</th>
+
+                    </tr>
+
+                </thead>
+                <!-- FIN DE LAS CABECERAS DE LA TABLA -->
+
+                <tbody>                                     
+                    
+                </tbody>
+            </table>
+        </div>
+        <!-- FIN DEL CONTENEDOR DE LA TABLA -->
+
+        <button type="button" name="btn-cargar-camion" id="btn-cargar-camion" class="btn btn-block btn-primary mt-2 mb-4" btn-lg btn-block">
+            <i class="fas fa-truck-loading">   </i> Iniciar Ruta  
+        </button>
+
+    </div>
+    <!-- FIN DEL CONTENEDOR PRINCIPAL DE LA VISTA -->
+
+    <!-- ENLACES HACIA LOS ESTILOS Y LAS FUNCIONALIDADES DEL DATATABLE -->
+    <link rel="stylesheet" type="text/css" href="resources/DataTables/dataTables.bootstrap4.min.css">
+
+    <script type="text/javascript" charset="utf8" src="resources/DataTables/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="resources/DataTables/datatables.min.js"></script>    
+    
+    <!-- ENLACE HACIA LOS ESTILOS Y LAS FUNCIONES JS -->
+    <script type="text/javascript" charset="utf8" src="resources/js/compras/logistica.js"></script>    
+
+</body>
+</html>
