2dac2253e869e5b298c08a69c14a38118e47d5e9
miguelmontiel30 <31743982+miguelmontiel30@users.noreply.github.com>
2019-05-22 09:45:43 -0500

    Arreglo en las librerías de DataTable
    * Se corrigió las rutas que causaban problemas.
    * Se agregó por separado la funcionalidad de la DT con el archivo "resources/compras/logistica.js".
    * Se creó una carpeta en resources (resources/compras)que contendrá los archivos JS para la funcionalidad de la DT.

--- compras/cargar_camion_log.php
+++ compras/cargar_camion_log.php
@@ -12,11 +12,8 @@
     <title><?php echo $_SESSION['nombreEmpresa']; ?></title>      
 
     <!-- LINK HACIA BOOTSTRAP, ESTILOS DE LA SIDENAV E ICONOS FONTAWESOME -->
-    <link href="../resources/css/Bootstrap 4/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="../resources/css/Bootstrap 4/css/all.min.css" rel="stylesheet">
-
-    <!-- ADICION DE LIBRERIA DATATABLE -->
-    <script language="javascript" type="text/javascript" src="../resources/js/jquery-3.3.1.min.js"></script>    
+    <link href="resources/css/Bootstrap 4/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="resources/css/Bootstrap 4/css/all.min.css" rel="stylesheet">
 
 </head>
 <body>
@@ -34,92 +31,32 @@
             
                     <tr>
 
+                        <th  class="text-center" scope="col">Fecha</th>
                         <th  class="text-center" scope="col">Ruta</th>
                         <th  class="text-center" scope="col">Destino</th>
                         <th  class="text-center" scope="col">Pedido</th>
                         <th  class="text-center" scope="col">Etiqueta</th>
+                        <th  class="text-center" scope="col">Clave de Embarque</th>
+                        <th  class="text-center" scope="col">No. de Control Vehicular</th>
                         <th  class="text-center" scope="col">Suma de Piezas</th>                        
-                        <th  class="text-center" scope="col">Piezas Dañadas</th>
+                        <th  class="text-center" scope="col">Piezas en camión</th>
+                        <th  class="text-center" scope="col">Piezas Entregadas</th>
+                        <th  class="text-center" scope="col">Status del pedido</th>
                         <th  class="text-center" scope="col">Cargar Camión</th>
-                        <th  class="text-center" scope="col">Observaciones</th>
 
                     </tr>
 
                 </thead>
                 <!-- FIN DE LAS CABECERAS DE LA TABLA -->
 
-                <tbody>
-
-                    <tr class="text-center">
-                        <th scope="row">23</th>
-                        <td>Puebla</td>
-                        <td>21345655431</td>
-                        <td>TO41531000631222002</td>
-                        <td>7</td>
-                        <td>0</td>
-                        <td>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="validar">                            
-                                <label class="custom-control-label" for="validar">Cargar Camión</label>
-                            </div>
-                        </td>
-                        <td></td>
-                    </tr> 
-                                       
-                    <tr class="text-center">
-                        <th scope="row">23</th>
-                        <td>Tulancingo</td>
-                        <td>1032423556</td>
-                        <td>TO415323245675431</td>
-                        <td>12</td>
-                        <td>2</td>
-                        <td>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="validar">                            
-                                <label class="custom-control-label" for="validar">Cargar Camión</label>
-                            </div>
-                        </td>
-                        <td></td>
-                    </tr>    
-
-                    <tr class="text-center">
-                        <th scope="row">23</th>
-                        <td>CDMX</td>
-                        <td>23323454</td>
-                        <td>TO415313245406622001</td>
-                        <td>50</td>
-                        <td>5</td>
-                        <td>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="validar">                            
-                                <label class="custom-control-label" for="validar">Cargar Camión</label>
-                            </div>
-                        </td>
-                        <td></td>
-                    </tr>     
-                                        
-                    <tr class="text-center">
-                        <th scope="row">23</th>
-                        <td>Guadalajara</td>
-                        <td>1000606622</td>
-                        <td>TO41531000606622001</td>
-                        <td>25</td>
-                        <td>2</td>
-                        <td>
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="validar">                            
-                                <label class="custom-control-label" for="validar">Cargar Camión</label>
-                            </div>
-                        </td>
-                        <td></td>
-                    </tr>                    
+                <tbody>                                     
                     
                 </tbody>
             </table>
         </div>
         <!-- FIN DEL CONTENEDOR DE LA TABLA -->
 
-        <button type="button" name="btn-cargar-camion" id="btn-cargar-camion" class="btn btn-block btn-success mt-2 mb-4" btn-lg btn-block">
+        <button type="button" name="btn-cargar-camion" id="btn-cargar-camion" class="btn btn-block btn-primary mt-2 mb-4" btn-lg btn-block">
             <i class="fas fa-truck-loading">   </i> Iniciar Ruta  
         </button>
 
@@ -127,13 +64,13 @@
     <!-- FIN DEL CONTENEDOR PRINCIPAL DE LA VISTA -->
 
     <!-- ENLACES HACIA LOS ESTILOS Y LAS FUNCIONALIDADES DEL DATATABLE -->
-    <link rel="stylesheet" type="text/css" href="../resources/DataTables/datatables.min.css">
-    <script type="text/javascript" charset="utf8" src="../resources/DataTables/datatables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="resources/DataTables/dataTables.bootstrap4.min.css">
 
-<script>
-    $(document).ready( function () {
-        $('#tabla_rutas').DataTable();
-    } );
-</script>    
+    <script type="text/javascript" charset="utf8" src="resources/DataTables/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="resources/DataTables/datatables.min.js"></script>    
+    
+    <!-- ENLACE HACIA LOS ESTILOS Y LAS FUNCIONES JS -->
+    <script type="text/javascript" charset="utf8" src="resources/js/compras/logistica.js"></script>    
+
 </body>
 </html>
